c085d8e4d156b6a2c8a91b76ab4c4b55161882cb
tianyou shen <sdlyingyong@163.com>
2018-08-28 17:10:29 +0800

    add okphp core

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// the DB config
+$config['db']['host'] = 'localhost';
+$config['db']['username'] = 'youruername';
+$config['db']['password'] = 'yourpassword';
+$config['db']['dbname'] = 'youdbname';
+
+// default controller and action
+$config['defaultController'] = 'Item';
+$config['defaultAction'] = 'index';
+
+return $config;
--- public/index.php
+++ public/index.php
@@ -1,6 +1,21 @@
 <?php
-/**
- * User: tianyou
- * Date: 2018\8\27 0027
- * msg:  the index of OKPHP
- */
+
+// Application directory is the current directory
+//应用目录为当前目录
+defind('APP_PATH', __DIR__.'/');
+
+// turn on the debug
+//开启调试模式
+defind('APP_DEBUG', true);
+
+// load framework file
+//加载框架文件
+require APP_PATH.'okphp/okphp.php';
+
+//load config file
+//加载配置文件
+$config = require APP_PATH.'config/config.php';
+
+//make the framework run
+//实例化框架
+(new okphp\okphp($config))->run();
